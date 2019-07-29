63428d6ac95383dd0c369098dc562b171108ed06
spatel13 <sahil.patel2461@gmail.com>
2019-07-29 16:45:05 -0400

    Adds vimrc, changes gitconfig, and refactors init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,166 +1,207 @@
-(menu-bar-mode 1) 
-(toggle-scroll-bar -1) 
-(tool-bar-mode -1) 
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
 (global-linum-mode 1)
 
 (setq debug-on-error t)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
 (setq show-paren-mode t)
+;; this ensures that I am not asked to confirm that the theme can be loaded
+(setq custom-safe-themes t)
 
 (require 'package)
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
+(setq package-archives '(
+			 ("elpa" . "https://elpa.gnu.org/packages/")
+			 ("melpa-stable" . "http://stable.melpa.org/packages/")
+			 ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 
-; list the packages you want
-(setq package-list '(god-mode writeroom-mode ace-jump-mode anaconda-mode dash dracula-theme f flx flx-ido ghub git-commit ido-completing-read+ ido-vertical-mode let-alist magit magit-popup memoize org org-grep py-autopep8 with-editor indent-guide powerline neotree all-the-icons flycheck))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-; fetch the list of packages available 
-(unless package-archive-contents
-  (package-refresh-contents))
+(use-package async
+  :ensure t
+  :config
+  (progn
+    async-bytecomp-package-mode t))
 
-; install the missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-(global-flycheck-mode)
-
-(load "indent-guide") ;; best not to include the ending “.el” or “.elc”
-(indent-guide-global-mode)
-
-(load "powerline")
-(powerline-default-theme)
-
-(autoload
-  'ace-jump-mode
-  "ace-jump-mode"
-  "Emacs quick move minor mode"
-  t)
-
-(require 'god-mode)
-(defun my-update-cursor ()
-  (setq cursor-type (if (or god-local-mode buffer-read-only)
-                        'box
-                      'bar)))
-
-(add-hook 'god-mode-enabled-hook 'my-update-cursor)
-(add-hook 'god-mode-disabled-hook 'my-update-cursor)
-(global-set-key (kbd "<escape>") 'god-local-mode)
-
-;;; SPECIAL SYNTAX HIGHLIGHTING
-;; Python operator and integer highlighting
-(require 'python)
-(add-hook 'python-mode-hook 'anaconda-mode)
-(font-lock-add-keywords 'python-mode
-   ; adds object and str and fixes it so that keywords that
-; often appear with : are assigned as builtin-face	
-   '(("\\<\\(object\\|str\\|else\\|except\\|finally\\|try\\|\\)\\>" 0 'font-lock-keyword-face)
- ;;("\\<[\\+-]?[0-9]+\\(.[0-9]+\\)?\\>" 0 'font-lock-constant-face)
- ("\\<[\\+-]?[0-9]+\\(.[0-9]+\\)?+\\(j+\\)?\\>" 0 'font-lock-constant-face) 
-   ("\\([][{}()~^<>:!|&=,.\\+*/%-]\\)" 0 'warning)
-   )) ;; end font-lock-add-keywords
-
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+(use-package dracula-theme
+  :ensure t)
 
 ;;; AUTO REFRESH PACKAGES
 (when (not package-archive-contents)
     (package-refresh-contents))
 
-;;; IDO CONFIG SECTION
-(require 'flx-ido)
-(require 'ido-vertical-mode)
-(require 'ido-completing-read+)
-(ido-mode 1)
-(ido-vertical-mode 1)
-(flx-ido-mode 1)
-(ido-ubiquitous-mode 1)
-(setq ido-enable-flex-matching t)
-(setq ido-use-face-nil t)
-(setq ido-everywhere t)
-(setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
-(setq ido-vertical-show-count t)
+(use-package indent-guide
+  :ensure t
+  :config
+  (progn
+    (indent-guide-global-mode)))
 
-(global-set-key
- "\M-x"
- (lambda ()
-   (interactive)
-   (call-interactively
-    (intern
-     (ido-completing-read
-      "M-x "
-      (all-completions "" obarray 'commandp))))))
+(use-package powerline
+  :ensure t
+  :config
+  (progn
+    (powerline-default-theme)))
 
-;; stop ido suggestion when doing a save-as
-(define-key (cdr ido-minor-mode-map-entry) [remap write-file] nil)
+;;; SPECIAL SYNTAX HIGHLIGHTING
+;; Python operator and integer highlighting
+(use-package python
+  :mode ("\\.py\\'" . python-mode)
+  :interpreter ("python" . python-mode)
+  :ensure t
+  :config
+  (progn
+    (add-hook 'python-mode-hook 'anaconda-mode)
+    (add-hook 'python-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)
+            (setq python-indent 4)))
+    (font-lock-add-keywords 'python-mode
+					; adds object and str and fixes it so that keywords that
+					; often appear with : are assigned as builtin-face	
+			    '(("\\<\\(object\\|str\\|else\\|except\\|finally\\|try\\|\\)\\>" 0 'font-lock-keyword-face)
+			      ;;("\\<[\\+-]?[0-9]+\\(.[0-9]+\\)?\\>" 0 'font-lock-constant-face)
+			      ("\\<[\\+-]?[0-9]+\\(.[0-9]+\\)?+\\(j+\\)?\\>" 0 'font-lock-constant-face) 
+			      ("\\([][{}()~^<>:!|&=,.\\+*/%-]\\)" 0 'warning)
+			      )) ;; end font-lock-add-keywords
+    ))
+
+(use-package py-autopep8
+  :ensure t
+  :config
+  (progn
+    (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)))
 
 (electric-pair-mode t)
-
-(global-set-key (kbd "C-c <left>")  'windmove-left)
-(global-set-key (kbd "C-c <right>") 'windmove-right)
-(global-set-key (kbd "C-c <up>")    'windmove-up)
-(global-set-key (kbd "C-c <down>")  'windmove-down)
-
-;; you can select the key you prefer to
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
-
-;; 
-;; enable a more powerful jump back function from ace jump mode
-;;
-;; https://github.com/winterTTr/ace-jump-mode/wiki/AceJump-FAQ
-;;
-(autoload
-  'ace-jump-mode-pop-mark
-  "ace-jump-mode"
-  "Ace jump back:-)"
-  t)
-(eval-after-load "ace-jump-mode"
-  '(ace-jump-mode-enable-mark-sync))
-(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (dracula)))
+ '(custom-enabled-themes '(dracula))
  '(custom-safe-themes
-   (quote
-    ("a4df5d4a4c343b2712a8ed16bc1488807cd71b25e3108e648d4a26b02bc990b3" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
+   '("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "a4df5d4a4c343b2712a8ed16bc1488807cd71b25e3108e648d4a26b02bc990b3" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default))
  '(package-selected-packages
-   (quote
-    (vue-html-mode rjsx-mode all-the-icons neotree powerline indent-guide py-autopep8 smart-tabs-mode writeroom-mode org-grep magit ido-vertical-mode ido-completing-read+ god-mode flx-ido dracula-theme auto-complete anaconda-mode ace-jump-mode)))
+   '(ob-async emacs-async protobuf-mode dockerfile-mode flycheck ivy-hydra counsel-projectile projectile use-package counsel ivy magit git-commit ghub writeroom-mode vue-html-mode rjsx-mode powerline indent-guide py-autopep8 smart-tabs-mode org-grep ido-vertical-mode ido-completing-read+ god-mode flx-ido dracula-theme auto-complete anaconda-mode ace-jump-mode))
  '(whitespace-style
-   (quote
-    (face trailing tabs spaces lines newline empty indentation::space space-before-tab space-mark tab-mark newline-mark))))
+   '(face trailing tabs spaces lines newline empty indentation::space space-before-tab space-mark tab-mark newline-mark)))
+
+(setq require-final-newline t)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 105 :family "Hack")))))
+ )
 
-(setq require-final-newline t)
+(defun bjm-swiper-recenter (&rest args)
+  "recenter display after swiper"
+  (recenter))
 
-(global-set-key (kbd "C-x g") 'magit-status)
+(use-package ivy
+  :ensure t
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (define-key ivy-minibuffer-map (kbd "C-c j") 'ivy-immediate-done)
+    (setq ivy-height 10)
+    (setq ivy-count-format "(%d/%d) ")
+    (setq ivy-display-style 'fancy)
+    (setq ivy-virtual-abbreviate 'full)
+    (setq magit-completing-read-function 'ivy-completing-read)
+    (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+    (define-key ivy-minibuffer-map (kbd "C-c j") 'ivy-immediate-done)
+    (global-set-key (kbd "C-c C-r") 'ivy-resume)
+    (global-set-key (kbd "C-j") 'ivy-switch-buffer)
+    (add-hook 'view-mode-hook
+              (function (lambda ()
+                          (define-key view-mode-map (kbd "C-j") 'ivy-switch-buffer))))))
 
-(require 'org)
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+(use-package counsel
+  :ensure t
+  :config
+  (progn
+    (global-set-key (kbd "C-c i m") 'counsel-imenu)
+    (global-set-key (kbd "M-x") 'counsel-M-x)
+    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+    (global-set-key (kbd "C-h f") 'counsel-describe-function)
+    (global-set-key (kbd "C-j") 'ivy-switch-buffer)
+    (global-set-key (kbd "M-y") 'counsel-yank-pop)
+    (global-set-key (kbd "M-l") 'counsel-bookmark)))
 
-(add-hook 'python-mode-hook
-          (lambda ()
-            (setq indent-tabs-mode nil)
-            (setq python-indent 4)))
+  
+;; good bye helm, hello swiper
+(use-package swiper
+  :ensure t
+  :config
+  (progn
+    (global-set-key "\C-s" 'swiper)
+    (global-set-key "\C-r" 'swiper)
+    (advice-add 'swiper :after #'bjm-swiper-recenter)))
 
-(require 'neotree)
- (global-unset-key "\C-\\")
-(global-set-key (kbd "C-\\") 'neotree-toggle)
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(use-package projectile
+  :ensure t
+  :config
+  (progn
+    (projectile-global-mode)
+    (setq projectile-enable-caching nil)
+    (global-set-key (kbd "C-f") 'projectile-command-map)
+    (setq projectile-completion-system 'ivy)
+    ;; (require 'counsel-projectile)
+    (setq projectile-switch-project-action 'projectile-dired)
+    (bind-key* "C-f l" 'projectile-find-file-other-window)
+    ;; use counsel-ag instead of projectile-ag
+    (defalias 'projectile-ag 'counsel-ag)))
+
+;; i <3 projectile
+(use-package counsel-projectile
+  :ensure t
+  :config
+  (progn
+    (defalias 'projectile-ag 'counsel-ag)
+    (global-set-key (kbd "C-f p") 'counsel-projectile-switch-project)
+    (setq counsel-projectile-switch-project-action 'counsel-projectile-switch-project-action-vc)))
+
+(use-package ivy-hydra
+  :ensure t)
+
+(use-package
+  flycheck
+  :ensure t
+  :init
+  (progn
+    (add-hook 'after-init-hook #'global-flycheck-mode)
+    (setq flycheck-highlighting-mode 'lines)))
+
+;; this is for magit
+(use-package magit
+  :ensure t
+  :config
+  (global-set-key (kbd "C-`") 'magit-status))
+
+;; start magit in full frame rather than split the frame
+(setq magit-display-buffer-function
+      (lambda (buffer)
+        (display-buffer
+         buffer (if (and (derived-mode-p 'magit-mode)
+                         (memq (with-current-buffer buffer major-mode)
+                               '(magit-process-mode
+                                 magit-revision-mode
+                                 magit-diff-mode
+                                 magit-stash-mode
+                                 magit-status-mode)))
+                    nil
+                  '(display-buffer-same-window)))))
+
+(use-package dockerfile-mode
+  :ensure t
+  :config
+  (progn
+    (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))))
---
+++ .vimrc
@@ -0,0 +1,340 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+"
+" Sections:
+"    -> Vundle
+"    -> General
+"    -> VIM user interface
+"    -> Colors and Fonts
+"    -> Files and backups
+"    -> Text, tab and indent related
+"    -> Visual mode related
+"    -> Moving around, tabs and buffers
+"    -> Status line
+"    -> Editing mappings
+"    -> vimgrep searching and cope displaying
+"    -> Spell checking
+"    -> Misc
+"    -> Helper functions
+"
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+""""""""""""""""""""""""""""""
+" => Vundle
+""""""""""""""""""""""""""""""
+
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+" let Vundle manage Vundle, required
+Plugin 'VundleVim/Vundle.vim'
+
+Plugin 'vim-airline/vim-airline'
+
+Plugin 'dracula/vim'
+
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+
+set runtimepath^=~/.vim/bundle/ctrlp.vim
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" => General
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Sets how many lines of history VIM has to remember
+set history=500
+
+" Enable filetype plugins
+filetype plugin on
+filetype indent on
+
+" Set to auto read when a file is changed from the outside
+set autoread
+
+" With a map leader it's possible to do extra key combinations
+" like <leader>w saves the current file
+let mapleader = ","
+
+" Fast saving
+nmap <leader>w :w!<cr>
+
+" :W sudo saves the file 
+" (useful for handling the permission-denied error)
+command W w !sudo tee % > /dev/null
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" => VIM user interface
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Set 7 lines to the cursor - when moving vertically using j/k
+set so=7
+
+" Turn on Wild menu
+set wildmenu
+
+" Always show current position
+set ruler
+
+" Heigh of the command bar
+set cmdheight=2
+
+" A buffer becomes hidden when it is abandoned
+set hid
+
+" Configure backspace so it acts as it should act
+set backspace=eol,start,indent
+set whichwrap+=<,>,h,l
+
+" Ignore case when searching
+set ignorecase
+
+" When searching try to be smart about cases 
+set smartcase
+
+" Highlight search results
+set hlsearch
+
+" Makes search act like search in modern browsers
+set incsearch 
+
+" Don't redraw while executing macros (good performance config)
+set lazyredraw 
+
+" For regular expressions turn magic on
+set magic
+
+" Show matching brackets when text indicator is over them
+set showmatch 
+" How many tenths of a second to blink when matching brackets
+set mat=2
+
+" No annoying sound on errors
+set noerrorbells
+set novisualbell
+set t_vb=
+set tm=500
+
+" Add a bit extra margin to the left
+set foldcolumn=1
+
+set clipboard=unnamed
+set relativenumber
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" => Colors and Fonts
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Enable syntax highlighting
+syntax enable 
+
+" Enable 256 colors palette in Gnome Terminal
+if $COLORTERM == 'gnome-terminal'
+    set t_Co=256
+endif
+
+try
+    colorscheme desert
+catch
+endtry
+
+set background=dark
+
+" Set extra options when running in GUI mode
+if has("gui_running")
+    set guioptions-=T
+    set guioptions-=e
+    set t_Co=256
+    set guitablabel=%M\ %t
+endif
+
+" Set utf8 as standard encoding and en_US as the standard language
+set encoding=utf8
+
+" Use Unix as the standard file type
+set ffs=unix,dos,mac
+
+" Airline config
+let g:airline_powerline_fonts = 1
+let g:airline_theme='dracula'
+let g:airline#extensions#tabline#enabled = 1
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" => Files, backups and undo
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Turn backup off, since most stuff is in SVN, git et.c anyway...
+set nobackup
+set nowb
+set noswapfile
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" => Text, tab and indent related
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Use spaces instead of tabs
+set expandtab
+
+" Be smart when using tabs ;)
+set smarttab
+
+" 1 tab == 4 spaces
+set shiftwidth=4
+set tabstop=4
+
+" Linebreak on 500 characters
+set lbr
+set tw=500
+
+set ai "Auto indent
+set si "Smart indent
+set wrap "Wrap lines
+
+
+""""""""""""""""""""""""""""""
+" => Visual mode related
+""""""""""""""""""""""""""""""
+" Visual mode pressing * or # searches for the current selection
+" Super useful! From an idea by Michael Naumann
+vnoremap <silent> * :<C-u>call VisualSelection('', '')<CR>/<C-R>=@/<CR><CR>
+vnoremap <silent> # :<C-u>call VisualSelection('', '')<CR>?<C-R>=@/<CR><CR>
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" => Moving around, tabs, windows and buffers
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+""""""""""""""""""""""""""""""
+" => Status line
+""""""""""""""""""""""""""""""
+" Always show the status line
+set laststatus=2
+
+" Format the status line
+set statusline=\ %{HasPaste()}%F%m%r%h\ %w\ \ CWD:\ %r%{getcwd()}%h\ \ \ Line:\ %l\ \ Column:\ %c
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" => Editing mappings
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Remap arrow keys in normal mode to yell at me to use hjkl
+nnoremap <Left> :echo "Use h"<CR>
+nnoremap <Right> :echo "Use l"<CR>
+nnoremap <Up> :echo "Use k"<CR>
+nnoremap <Down> :echo "Use j"<CR>
+
+" Add Ctrl+[jk] to jump up and down 15 lines
+nnoremap <C-j> 10jzz
+nnoremap <C-k> 10kzz
+
+" Remap VIM 0 to first non-blank character
+map 0 ^
+
+" Move a line of text using ALT+[jk] or Command+[jk] on mac
+nmap <M-j> mz:m+<cr>`z
+nmap <M-k> mz:m-2<cr>`z
+vmap <M-j> :m'>+<cr>`<my`>mzgv`yo`z
+vmap <M-k> :m'<-2<cr>`>my`<mzgv`yo`z
+
+if has("mac") || has("macunix")
+  nmap <D-j> <M-j>
+  nmap <D-k> <M-k>
+  vmap <D-j> <M-j>
+  vmap <D-k> <M-k>
+endif
+
+" Delete trailing white space on save, useful for some filetypes ;)
+fun! CleanExtraSpaces()
+    let save_cursor = getpos(".")
+    let old_query = getreg('/')
+    silent! %s/\s\+$//e
+    call setpos('.', save_cursor)
+    call setreg('/', old_query)
+endfun
+
+if has("autocmd")
+    autocmd BufWritePre *.txt,*.js,*.py,*.wiki,*.sh,*.coffee :call CleanExtraSpaces()
+endif
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" => Spell checking
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Pressing ,ss will toggle and untoggle spell checking
+map <leader>ss :setlocal spell!<cr>
+
+" Shortcuts using <leader>
+map <leader>sn ]s
+map <leader>sp [s
+map <leader>sa zg
+map <leader>s? z=
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" => Misc
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Remove the Windows ^M - when the encodings gets messed up
+noremap <Leader>m mmHmt:%s/<C-V><cr>//ge<cr>'tzt'm
+
+" Quickly open a buffer for scribble
+map <leader>q :e ~/buffer<cr>
+
+" Quickly open a markdown buffer for scribble
+map <leader>x :e ~/buffer.md<cr>
+
+" Toggle paste mode on and off
+map <leader>pp :setlocal paste!<cr>
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" => Helper functions
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Returns true if paste mode is enabled
+function! HasPaste()
+    if &paste
+        return 'PASTE MODE  '
+    endif
+    return ''
+endfunction
+
+" Don't close window, when deleting a buffer
+command! Bclose call <SID>BufcloseCloseIt()
+function! <SID>BufcloseCloseIt()
+    let l:currentBufNum = bufnr("%")
+    let l:alternateBufNum = bufnr("#")
+
+    if buflisted(l:alternateBufNum)
+        buffer #
+    else
+        bnext
+    endif
+
+    if bufnr("%") == l:currentBufNum
+        new
+    endif
+
+    if buflisted(l:currentBufNum)
+        execute("bdelete! ".l:currentBufNum)
+    endif
+endfunction
+
+function! CmdLine(str)
+    call feedkeys(":" . a:str)
+endfunction 
+
+function! VisualSelection(direction, extra_filter) range
+    let l:saved_reg = @"
+    execute "normal! vgvy"
+
+    let l:pattern = escape(@", "\\/.*'$^~[]")
+    let l:pattern = substitute(l:pattern, "\n$", "", "")
+
+    if a:direction == 'gv'
+        call CmdLine("Ack '" . l:pattern . "' " )
+    elseif a:direction == 'replace'
+        call CmdLine("%s" . '/'. l:pattern . '/')
+    endif
+
+    let @/ = l:pattern
+    let @" = l:saved_reg
+endfunction
+
